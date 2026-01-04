e6bae570f7de65f8df45185d1a41b5975dcfbf5b
RSChao <rschao@outlook.com>
2026-01-04 17:21:20 +0100

    Add summary

--- src/main/java/com/rschao/plugins/techniqueAPI/TechAPI.java
+++ src/main/java/com/rschao/plugins/techniqueAPI/TechAPI.java
@@ -1,17 +1,26 @@
 package com.rschao.plugins.techniqueAPI;
 
+import com.rschao.plugins.techniqueAPI.tech.register.TechRegistry;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public final class TechAPI extends JavaPlugin {
 
+    public static final Logger LOGGER = Logger.getLogger("TechniqueAPI");
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        LOGGER.info("TechniqueAPI enabled!");
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
+            TechRegistry.summarizeTechniques();
+        }, 20L);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        LOGGER.info("TechniqueAPI disabled!");
     }
 }
