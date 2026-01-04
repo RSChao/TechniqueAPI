9cd3c53cb0a987a9fb81ea6279eef7b05fdc63e1
RSChao <rschao@outlook.com>
2026-01-04 16:31:30 +0100

    Fix typo

--- src/main/java/com/rschao/plugins/techniqueAPI/tech/util/PreRunUtil.java
+++ src/main/java/com/rschao/plugins/techniqueAPI/tech/util/PreRunUtil.java
@@ -5,7 +5,7 @@ import com.rschao.plugins.techniqueAPI.tech.Technique;
 import com.rschao.plugins.techniqueAPI.tech.context.TechniqueContext;
 
 public class PreRunUtil {
-    public static PreRunHook allwaysTrue() {
+    public static PreRunHook alwaysTrue() {
         return new PreRunHook() {
             @Override
             public boolean run(TechniqueContext ctx, Technique tech) {
